39f8d1026d8013316c3e1dc8ab570a17eba71a50
Marco Moschettini <m.marmos@gmail.com>
2023-08-09 18:20:32 +0200

    add basic swc example

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = 8080;
+
+app.get('/', (_req: Request, res: Response) =>
+{
+	res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () =>
+{
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
